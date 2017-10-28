0bc3e90590ec8391f8ed6dee2702be425cc2cfb3
efrain <carlosefrain17@hotmail.com>
2017-10-28 16:06:07 -0500

    ejercicio tres con factor de suma

--- app/Encryption/Encryption.js
+++ app/Encryption/Encryption.js
@@ -6,17 +6,65 @@ class Encryption extends Component {
     this.state = {};
   }
 
-  desencripta(palabra, fila) {
+  desencripta(palabra, fila, factor) {
     let arrayFila = fila.split('');
     let arrayPalabra = palabra.split('');
     let letraIndice;
     let palabraDescrip = '';
     let indice;
     arrayPalabra.map(letra => {
-      letraIndice = arrayFila.indexOf(letra) + 12;
+      letraIndice = arrayFila.indexOf(letra) + factor;
       indice = letraIndice > 25 ? letraIndice - 26: letraIndice;
       palabraDescrip = palabraDescrip + arrayFila[indice];
     });
+    return palabraDescrip;
+  }
+
+  optenerFila(numero) {
+    let tabla = [
+      'BCDEFGHIJKLMNOPQRSTUVWXYZA',
+      'CDEFGHIJKLMNOPQRSTUVWXYZAB',
+      'DEFGHIJKLMNOPQRSTUVWXYZABC',
+      'EFGHIJKLMNOPQRSTUVWXYZABCD',
+      'FGHIJKLMNOPQRSTUVWXYZABCDE',
+      'GHIJKLMNOPQRSTUVWXYZABCDEF',
+      'HIJKLMNOPQRSTUVWXYZABCDEFG',
+      'IJKLMNOPQRSTUVWXYZABCDEFGH',
+      'JKLMNOPQRSTUVWXYZABCDEFGHI',
+      'KLMNOPQRSTUVWXYZABCDEFGHIJ',
+      'LMNOPQRSTUVWXYZABCDEFGHIJK',
+      'MNOPQRSTUVWXYZABCDEFGHIJKL',
+      'NOPQRSTUVWXYZABCDEFGHIJKLM',
+      'OPQRSTUVWXYZABCDEFGHIJKLMN',
+      'PQRSTUVWXYZABCDEFGHIJKLMNO',
+      'QRSTUVWXYZABCDEFGHIJKLMNOP',
+      'RSTUVWXYZABCDEFGHIJKLMNOPQ',
+      'STUVWXYZABCDEFGHIJKLMNOPQR',
+      'TUVWXYZABCDEFGHIJKLMNOPQRS',
+      'UVWXYZABCDEFGHIJKLMNOPQRST',
+      'VWXYZABCDEFGHIJKLMNOPQRSTU',
+      'WXYZABCDEFGHIJKLMNOPQRSTUV',
+      'XYZABCDEFGHIJKLMNOPQRSTUVW',
+      'YZABCDEFGHIJKLMNOPQRSTUVWX',
+      'ZABCDEFGHIJKLMNOPQRSTUVWXY'
+    ]
+    return tabla[numero - 1];
+  }
+
+  desencriptarMensajeComplejo(filas, mensaje) {
+    let arrayfilas = filas.split(' ');
+    let arrayMensaje = mensaje.split(' ');
+    let fila;
+    let palabraDescrip = '';
+    let factor = 0;
+    let espacio;
+
+    for (let i = 0; i < arrayMensaje.length; i++) {
+      fila = this.optenerFila(arrayfilas[i]);
+      factor = 26 - parseInt(arrayfilas[i]);
+      espacio = i == arrayMensaje.length - 1 ? '': ' ';
+      palabraDescrip = palabraDescrip + this.desencripta(arrayMensaje[i], fila, factor) + espacio;
+    }
     return palabraDescrip;
   }
   
--- app/Encryption/specs/Encryption.spec.js
+++ app/Encryption/specs/Encryption.spec.js
@@ -19,13 +19,24 @@ describe('Encryption', () => {
 
   describe('desencriptador', () => {
     it('desencripta Batman', () => {
-      const palabra = instance.desencripta('POHAOB', 'OPQRSTUVWXYZABCDEFGHIJKLMN');
+      const palabra = instance.desencripta('POHAOB', 'OPQRSTUVWXYZABCDEFGHIJKLMN', 12);
       expect(palabra).toBe('BATMAN');
     });
 
-     it('desencripta Batman', () => {
-      const palabra = instance.desencripta('VCZO', 'OPQRSTUVWXYZABCDEFGHIJKLMN');
+     it('desencripta HOLA', () => {
+      const palabra = instance.desencripta('VCZO', 'OPQRSTUVWXYZABCDEFGHIJKLMN', 12);
       expect(palabra).toBe('HOLA');
+    });
+
+    it('Obtiene fila correcta', () => {
+      const fila = instance.optenerFila(3);
+      expect(fila).toBe('DEFGHIJKLMNOPQRSTUVWXYZABC');
+    });
+
+    it('desencripta mensaje complejo', () => {
+      let mensaje = { fila: '1 9', mensaje: 'FTUBT LNALJ' };
+      const fila = instance.desencriptarMensajeComplejo('1 9', 'FTUBT LNALJ');
+      expect(fila).toBe('ESTAS CERCA');
     });
 
   });
